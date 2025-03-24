2a0ce5e3e40fecdd3abc527209036e3e34110fd3
钱首熠 <qianshouyi@58.com>
2025-03-24 20:40:43 +0800

    处理状态

--- app/script/update_exists_state.py
+++ app/script/update_exists_state.py
@@ -5,7 +5,7 @@ from app.model.list_task import ListTask
 from app.model.spider_list_config import SpiderListConfig
 from app.tasks.tasks_gather import spider_list, extract_list
 from app.io.session import engine
-from sqlmodel import Session, select, update, func, or_
+from sqlmodel import Session, select, update, func, or_, and_
 import json
 
 # 任务执行顺序: 列表A-1页——>2页
@@ -33,6 +33,58 @@ def update_title_trans():
                 print(k)
         db.commit()
 
+def update_title_trans():
+    # 查询基础信息
+    with Session(engine, autoflush=False) as db:
+        smt = select(NewsDetail).where(NewsDetail.title_translate == None)
+        exist_data = db.exec(smt).all()
+        print(len(exist_data))
+        k = 0
+        for temp in exist_data:
+            # 查询具体配置
+            smt_one = select(ListTask).where(ListTask.id == temp.unique_id)
+            exist_one = db.exec(smt_one).one_or_none()
+            if not exist_one:
+                continue
+            else:
+                # 序列化列表参数
+                temp_title_trans = exist_one.title_translate
+                temp.title_translate = temp_title_trans
+                db.add(temp)
+                k += 1
+            if k%10 ==0:
+                print(k)
+        db.commit()
+
+def update_main_classify():
+    # 查询基础信息
+    with Session(engine, autoflush=False) as db:
+        smt = select(NewsDetail).where(
+            or_(
+                NewsDetail.main_classify == None,
+                NewsDetail.main_classify == ""
+            )
+        )
+        exist_data = db.exec(smt).all()
+        print(len(exist_data))
+        k = 0
+        for temp in exist_data:
+            # 查询具体配置
+            smt_one = select(ListTask).where(ListTask.id == temp.unique_id)
+            exist_one = db.exec(smt_one).one_or_none()
+            if not exist_one:
+                continue
+            else:
+                # 序列化列表参数
+                if int(exist_one.tag) == 1:
+                    temp_main_classify = exist_one.main_classify
+                    temp.main_classify = temp_main_classify
+                    db.add(temp)
+            k += 1
+            if k%10 ==0:
+                print(k)
+        db.commit()
 if __name__ == "__main__":
-    print(update_title_trans())
+    update_title_trans()
+    update_main_classify()
     
