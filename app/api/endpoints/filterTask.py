8040aa48760f71f58aab45a5cdf0a62b4637c2dd
Ubuntu <ubuntu@localhost.localdomain>
2025-10-19 14:56:04 +0800

    更新

--- app/api/endpoints/filterTask.py
+++ app/api/endpoints/filterTask.py
@@ -105,11 +105,7 @@ async def endpoint(request: Request, db: Session = Depends(deps.get_db), ):
             t_start_date = t_end_date - timedelta(days=2)
         else:
             t_mid_date = datetime.strptime(str(datetime.now()).split(" ")[0], "%Y-%m-%d").date()
-<<<<<<< HEAD
             t_start_date = t_mid_date - timedelta(days=15)
-=======
-            t_start_date = t_mid_date - timedelta(days=90)
->>>>>>> 36eb0af197d3b62ba438bf9289eb620caf790be7
             t_end_date = t_mid_date + timedelta(days=1)
         
         # 过滤条件
